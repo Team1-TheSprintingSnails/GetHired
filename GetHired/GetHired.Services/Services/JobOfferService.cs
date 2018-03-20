34f1411d36887f066a9c00272e60b5fad67e046a
Petko <petkotihomirovpetkov@gmail.com>
2018-03-20 15:58:42 +0200

    Added some services

---
+++ GetHired/GetHired.Services/Services/JobOfferService.cs
@@ -0,0 +1,19 @@
+﻿using GetHired.DataModels.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHired.Services.Services
+{
+    class JobOfferService
+    {
+        private IUnitOfWork unitOfWork;
+
+        public JobOfferService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+    }
+}
---
+++ GetHired/GetHired.Services/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using GetHired.DataModels.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHired.Services.Services
+{
+    class UserService
+    {
+        private IUnitOfWork unitOfWork;
+
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+    }
+}
