f9dd99a06fd3bc431498860c9d88de659b824720
Petko <petkotihomirovpetkov@gmail.com>
2018-03-20 18:52:23 +0200

    Adding TownService

--- GetHired/GetHired.DTO/JobTypeModel.cs
+++ GetHired/GetHired.DTO/JobTypeModel.cs
@@ -1,8 +1,11 @@
-﻿namespace GetHired.DTO
+﻿using GetHired.Common.Mapping;
+using GetHired.DomainModels.Utilities;
+
+namespace GetHired.DTO
 {
     public class JobTypeModel
     {
         public string TypeName { get; set; }
-        
+
     }
 }
