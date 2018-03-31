3855e2043d54750549a444f2e7612310a607ff03
Petko <petkotihomirovpetkov@gmail.com>
2018-03-31 19:04:57 +0300

    Added all remaining tests for the CompanyService

---
+++ GetHired/GetHired.UnitTests/Services/CompanyServiceTestss/GetAll_Should.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using GetHired.DataModels.Contracts;
+using GetHired.DataModels.Repositories.Contracts;
+using GetHired.DomainModels;
+using GetHired.DTO;
+using GetHired.Services.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace GetHired.UnitTests.Services.CompanyServiceTestss
+{
+    [TestFixture]
+    public class GetAll_Should
+    {
+        [Test]
+        public void GetAllCitiesMappedToCityModels_WhenInvoked()
+        {
+            //Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMapper = new Mock<IMapper>();
+            var companyService = new CompanyService(mockedUnitOfWork.Object, mockedMapper.Object);
+            var mockedCompany = new Mock<Company>();
+            var mockedCompanyRepository = new Mock<IGenericRepository<Company>>();
+
+            //Act
+            mockedUnitOfWork.Setup(x => x.CompanyRepository).Returns(mockedCompanyRepository.Object);
+            mockedCompanyRepository.Setup(x => x.All).Returns(new List<Company>() { mockedCompany.Object });
+
+            //Assert
+            var collectionOfCompanies = new List<CompanyModel>(companyService.GetAll());
+            Assert.AreEqual(1, collectionOfCompanies.Count);
+        }
+    }
+}
---
+++ GetHired/GetHired.UnitTests/Services/CompanyServiceTestss/GetById_Should.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using GetHired.DataModels.Repositories.Contracts;
+using Moq;
+using GetHired.DomainModels;
+using GetHired.DataModels.Contracts;
+using AutoMapper;
+using GetHired.Services.Services;
+using System.Linq.Expressions;
+using GetHired.DTO;
+
+namespace GetHired.UnitTests.Services.CompanyServiceTestss
+{
+    [TestFixture]
+    public class GetById_Should
+    {
+        [Test]
+        public void MakeAllChainCalls_WhenInvoked()
+        {
+            //Arrange
+            var mockedCompanyModel = new Mock<CompanyModel>();
+            var mockedCompany = new Mock<Company>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMapper = new Mock<IMapper>();
+            var companyService = new CompanyService(mockedUnitOfWork.Object, mockedMapper.Object);
+            var mockedCompanyRepository = new Mock<IGenericRepository<Company>>();
+
+            //Act & Assert
+            mockedUnitOfWork.Setup(x => x.CompanyRepository).Returns(mockedCompanyRepository.Object);
+            mockedCompanyRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(mockedCompany.Object);
+            mockedMapper.Setup(x => x.Map<CompanyModel>(mockedCompany.Object)).Returns(mockedCompanyModel.Object);
+            companyService.GetById(1);
+            mockedMapper.Verify(x => x.Map<CompanyModel>(mockedCompany.Object), Times.Once);
+        }
+
+        [Test]
+        public void ReturnExpectedValue_WhenInvoked()
+        {
+            //Arrange
+            var mockedCompanyModel = new Mock<CompanyModel>();
+            var mockedCompany = new Mock<Company>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMapper = new Mock<IMapper>();
+            var companyService = new CompanyService(mockedUnitOfWork.Object, mockedMapper.Object);
+            var mockedCompanyRepository = new Mock<IGenericRepository<Company>>();
+
+            //Act & Assert
+            mockedUnitOfWork.Setup(x => x.CompanyRepository).Returns(mockedCompanyRepository.Object);
+            mockedCompanyRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(mockedCompany.Object);
+            mockedMapper.Setup(x => x.Map<CompanyModel>(mockedCompany.Object)).Returns(mockedCompanyModel.Object);
+            Assert.AreEqual(mockedCompanyModel.Object, companyService.GetById(1));
+        }
+    }
+}
