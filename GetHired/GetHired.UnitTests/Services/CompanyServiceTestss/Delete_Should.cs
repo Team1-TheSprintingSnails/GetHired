61e23f8cf5014e16bd0ec3c8ce08301b7d7de237
Petko <petkotihomirovpetkov@gmail.com>
2018-03-31 18:27:32 +0300

    Added test for DeleteById and Delete methods of CompanyService

---
+++ GetHired/GetHired.UnitTests/Services/CompanyServiceTestss/Delete_Should.cs
@@ -0,0 +1,124 @@
+﻿using AutoMapper;
+using GetHired.DataModels.Contracts;
+using GetHired.DataModels.Repositories.Contracts;
+using GetHired.DomainModels;
+using GetHired.DTO;
+using GetHired.Services.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace GetHired.UnitTests.Services.CompanyServiceTestss
+{
+    [TestFixture]
+    public class Delete_Should
+    {
+        [Test]
+        public void ReturnFalse_WhenNullArgumentIsPassed()
+        {
+            //Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMapper = new Mock<IMapper>();
+            var companyService = new CompanyService(mockedUnitOfWork.Object, mockedMapper.Object);
+
+            //Act & Assert
+            Assert.IsFalse(companyService.Delete(null));
+        }
+
+        [Test]
+        public void ReturnFalse_WhenExceptionOccured()
+        {
+            //Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMapper = new Mock<IMapper>();
+            var companyService = new CompanyService(mockedUnitOfWork.Object, mockedMapper.Object);
+            var mockedCompanyModel = new Mock<CompanyModel>();
+
+            //Act
+            mockedUnitOfWork.Setup(x => x.CompanyRepository).Throws(new Exception());
+
+            //Assert
+            Assert.IsFalse(companyService.Delete(mockedCompanyModel.Object));
+        }
+
+        [Test]
+        public void CallMapperMapMethod_WhenInvokedWithValidArgs()
+        {
+            //Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMapper = new Mock<IMapper>();
+            var companyService = new CompanyService(mockedUnitOfWork.Object, mockedMapper.Object);
+            var mockedCompany = new Mock<Company>();
+            var mockedCompanyModel = new Mock<CompanyModel>();
+            var mockedCompanyRepository = new Mock<IGenericRepository<Company>>();
+
+            //Act
+            mockedMapper.Setup(x => x.Map<Company>(mockedCompanyModel.Object)).Returns(mockedCompany.Object);
+            companyService.Delete(mockedCompanyModel.Object);
+
+            //Assert
+            mockedMapper.Verify(x => x.Map<Company>(mockedCompanyModel.Object), Times.Once);
+        }
+
+        [Test]
+        public void CallInheritedDeleteMethod_WhenInvokedWithValidArgs()
+        {
+            //Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMapper = new Mock<IMapper>();
+            var companyService = new CompanyService(mockedUnitOfWork.Object, mockedMapper.Object);
+            var mockedCompany = new Mock<Company>();
+            var mockedCompanyModel = new Mock<CompanyModel>();
+            var mockedCompanyRepository = new Mock<IGenericRepository<Company>>();
+
+            //Act
+            mockedMapper.Setup(x => x.Map<Company>(mockedCompanyModel.Object)).Returns(mockedCompany.Object);
+            mockedUnitOfWork.Setup(x => x.CompanyRepository).Returns(mockedCompanyRepository.Object);
+            companyService.Delete(mockedCompanyModel.Object);
+
+            //Assert
+            mockedCompanyRepository.Verify(x => x.Delete(mockedCompany.Object), Times.Once);
+        }
+
+        [Test]
+        public void CallSaveChangesMethod_WhenInvokedWithValidArgs()
+        {
+            //Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMapper = new Mock<IMapper>();
+            var companyService = new CompanyService(mockedUnitOfWork.Object, mockedMapper.Object);
+            var mockedCompany = new Mock<Company>();
+            var mockedCompanyModel = new Mock<CompanyModel>();
+            var mockedCompanyRepository = new Mock<IGenericRepository<Company>>();
+
+            //Act
+            mockedMapper.Setup(x => x.Map<Company>(mockedCompanyModel.Object)).Returns(mockedCompany.Object);
+            mockedUnitOfWork.Setup(x => x.CompanyRepository).Returns(mockedCompanyRepository.Object);
+            companyService.Delete(mockedCompanyModel.Object);
+
+            //Assert
+            mockedUnitOfWork.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [Test]
+        public void ExecuteMethodAndReturnTrue_WhenInvokedWithValidArgs()
+        {
+            //Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMapper = new Mock<IMapper>();
+            var companyService = new CompanyService(mockedUnitOfWork.Object, mockedMapper.Object);
+            var mockedCompany = new Mock<Company>();
+            var mockedCompanyModel = new Mock<CompanyModel>();
+            var mockedCompanyRepository = new Mock<IGenericRepository<Company>>();
+
+            //Act
+            mockedMapper.Setup(x => x.Map<Company>(mockedCompanyModel.Object)).Returns(mockedCompany.Object);
+            mockedUnitOfWork.Setup(x => x.CompanyRepository).Returns(mockedCompanyRepository.Object);
+            mockedCompanyRepository.Setup(x => x.Update(mockedCompany.Object));
+            mockedUnitOfWork.Setup(x => x.SaveChanges());
+
+            //Assert
+            Assert.IsTrue(companyService.Delete(mockedCompanyModel.Object));
+        }
+    }
+}
