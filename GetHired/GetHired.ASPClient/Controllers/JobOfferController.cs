c164fe017785a04ca158e7de9ee48699ddc1103f
Irina Hristova <hristova119@gmail.com>
2018-03-31 23:22:16 +0300

    Working on JSON loader

--- GetHired/GetHired.ASPClient/Controllers/JobOfferController.cs
+++ GetHired/GetHired.ASPClient/Controllers/JobOfferController.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using GetHired.DomainModels;
 using GetHired.Services.Contracts;
-using GetHired.Utils.Contracts;
+using Newtonsoft.Json;
 using Rotativa;
 
 namespace GetHired.ASPClient.Controllers
@@ -24,6 +28,46 @@ namespace GetHired.ASPClient.Controllers
         public ActionResult DownloadPDF()
         {
             return new ActionAsPdf("Index");
+        }
+
+        [HttpPost]
+        public ActionResult LoadFromJson(HttpPostedFileBase jsonFile)
+        {
+            if (!Path.GetFileName(jsonFile.FileName).EndsWith(".json"))
+            {
+
+            }
+            else
+            {
+                jsonFile.SaveAs(Server.MapPath("~/JSONFiles" + Path.GetFileName(jsonFile.FileName)));
+
+                StreamReader streamReader = new StreamReader(Server.MapPath("~/JSONFiles" + Path.GetFileName(jsonFile.FileName)));
+
+                string data = streamReader.ReadToEnd();
+
+                var jobOffers = JsonConvert.DeserializeObject<List<JobOffer>>(data);
+
+                jobOffers.ForEach(jo =>
+                {
+                    var jobOffer = new JobOffer()
+                    {
+                        Id = jo.Id,
+                        Position = jo.Position,
+                        Description = jo.Description,
+                        Payment = jo.Payment,
+                        CompanyId = jo.CompanyId,
+                        Rating = jo.Rating,
+                        JobType = jo.JobType,
+                        JobCategory = jo.JobCategory,
+                        Company = jo.Company,
+                        LikedBy = jo.LikedBy,
+                        DateModified = jo.DateModified,
+                        DateCreated = jo.DateCreated
+                    };
+                });
+            }
+
+            return View("Index");
         }
     }
 }
